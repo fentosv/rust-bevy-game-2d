ecda454304feb153fb443d66f8722a3bbe73f5b4
fentosv <fentos.dev@gmail.com>
2023-06-21 18:39:45 +0200

    Add variables.rs

---
+++ src/variables.rs
@@ -0,0 +1,5 @@
+// These pub constants are defined in `Transform` units.
+// Using the default 2D camera they correspond 1:1 with screen pixels.
+
+pub const PLAYER_SPEED: f32 = 10.0;
+// How close can the player get to the wall
