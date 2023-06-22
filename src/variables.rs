9a462098fb5a962797941d8e724be348d4b6b2b3
fentosv <fentos.dev@gmail.com>
2023-06-22 19:43:55 +0200

    Add variables

--- src/variables.rs
+++ src/variables.rs
@@ -1,5 +1,20 @@
+use bevy::prelude::*;
 // These pub constants are defined in `Transform` units.
 // Using the default 2D camera they correspond 1:1 with screen pixels.
 
 pub const PLAYER_SPEED: f32 = 10.0;
 // How close can the player get to the wall
+
+// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
+pub const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
+pub const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
+pub const BALL_SPEED: f32 = 400.0;
+pub const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+pub const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+
+pub const SCOREBOARD_FONT_SIZE: f32 = 40.0;
+pub const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+
+pub const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+pub const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+pub const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
